b02352c4e52c8854d19940fc66230cc3b462ec40
Ndatumuremyi paterne <60066777+ndatumuremyi@users.noreply.github.com>
2022-05-06 08:15:39 +0200

    signup

--- components/context/UserContext.js
+++ components/context/UserContext.js
@@ -1,14 +1,37 @@
 import {createContext, useState} from "react";
+import config from "@/config";
 
 const UserContext = createContext();
 export default UserContext;
 
+function handleSignUp({email,name, phone}){
+    fetch("http://localhost:2023/api/v1/auth/signup", {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            name,
+            phone:phone+""
+        }),
+        headers: {
+            'Content-type': 'application/json;'
+        }
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error);
+    });
+}
 export function UserProvider({children}){
     const userD = require('../assert/user.json');
     const [user, setUser] = useState({...userD});
 
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser, handleSignUp}}>
             {children}
         </UserContext.Provider>
     )
--- components/screens/signup/signup.js
+++ components/screens/signup/signup.js
@@ -1,24 +1,35 @@
 import {Image, Button} from "@chakra-ui/core";
 import classes from "./Css.module.css";
-
+import {useRef, useContext} from "react";
+import userContext from "@/components/context/UserContext";
 
 export default function Signup(){
+    const {handleSignUp} = useContext(userContext);
+    const email = useRef();
+    const name = useRef();
+    const phone = useRef();
+    const handleSubmit = () => {
+        handleSignUp({email:email.current.value,username:name.current.value,phone:phone.current.value});
+    }
     return(
         <div className={"container-fluid "}
         >
             <div className="row">
                 <div className="col">
                     <form className={"text-center " + classes.form} id="form">
-                        <h1 className={classes.head} id="head">Login</h1>
+                        <h1 className={classes.head} id="head">SignUp</h1>
                         <div><Image className={"rounded img-fluid "+classes.image }id="image" alt={""}
                                   src="logo.svg"/></div>
-                        <div className="form-group mb-3"><input className={"form-control "+classes.formum} type="email" id="emailControl"
+                        <div className="form-group mb-3"><input ref={email}
+                            onChange={() => {}}
+                            className={"form-control "+classes.formum} type="email" id="emailControl"
                                                                 placeholder="Email"/></div>
-                        <div className="form-group mb-3"><input className={"form-control "+classes.formum} type="text" id="usernameControl"
+                        <div className="form-group mb-3"><input ref={name} className={"form-control "+classes.formum} type="text" id="usernameControl"
                                                                 placeholder="Username"/></div>
-                        <div className="form-group mb-3"><input className={"form-control "+classes.formum} type="tel" id="phoneControl"
+                        <div className="form-group mb-3"><input ref={phone} className={"form-control "+classes.formum} type="tel" id="phoneControl"
                                                                 placeholder="phone"/></div>
                         <Button
+                            onClick={() => handleSubmit()}
                             as="a"
                             fontWeight="medium"
                             colorScheme='blue' variant='outline'
