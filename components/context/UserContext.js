98ce9125c49f38d2368454cce967e3d748783d4f
Ndatumuremyi paterne <60066777+ndatumuremyi@users.noreply.github.com>
2022-05-10 09:08:49 +0200

    add creation of user

--- components/context/UserContext.js
+++ components/context/UserContext.js
@@ -1,58 +1,70 @@
 import {createContext, useState} from "react";
 import config from "@/config";
+import axios from "axios";
 
 const UserContext = createContext();
 export default UserContext;
-function handleLogin({login,password}){
-    fetch(config.backendURL+"/api/v1/auth/login", {
-        method: 'POST',
-        body: JSON.stringify({
-            login,
-            password
-        }),
-        headers: {
-            'Content-type': 'application/json;'
-        }
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-        return Promise.reject(response);
-    }).then(function (data) {
-        console.log(data);
-    }).catch(function (error) {
-        console.warn('Something went wrong.', error);
-    });
-}
-function handleSignUp({email,name, phone}){
-    console.log(email+" "+ name +" " +phone)
-    fetch(config.backendURL+"/api/v1/auth/signup", {
-        method: 'POST',
-        body: JSON.stringify({
-            email,
-            name,
-            phone:phone+""
-        }),
-        headers: {
-            'Content-type': 'application/json;'
-        }
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-        return Promise.reject(response);
-    }).then(function (data) {
-        console.log(data);
-    }).catch(function (error) {
-        console.warn('Something went wrong.', error);
-    });
-}
+
 export function UserProvider({children}){
+
     const userD = require('../assert/user.json');
     const [user, setUser] = useState({...userD});
+    const [userCreation, setUserCreation] = useState({status:config.status.NOTHING,payload: {}});
+
+
+
+
+
+    async function handleSignUp(data){
+        console.log(JSON.stringify({...data}));
+        setUserCreation({...userCreation, status: config.status.LOADING});
+        return axios.post(config.backendURL+"/api/v1/auth/signup", {...data})
+            .then(function (response) {
+                if(response.status === 201 || response.status === 200){
+                    console.log(response.data);
+                    setUserCreation({payload: {...response.data}, status: config.status.DONE});
+                    return {payload: {...response}, status: config.status.DONE}
+                }
+                else {
+                    setUserCreation({payload: {...response.data}, status: config.status.ERROR});
+                    return {payload: {...response.data}, status: config.status.ERROR}
+                }
+
+            }).then((output) => {
+                return {...output}
+            })
+            .catch(function (error) {
+                console.log("before error")
+                console.log(error.response.data);
+                setUserCreation({payload: {...error.response.data}, status: config.status.ERROR});
+                return {payload: {...error.response.data}, status: config.status.ERROR}
+            });
+    }
+
+    function handleLogin({login,password}){
+        fetch(config.backendURL+"/api/v1/auth/login", {
+            method: 'POST',
+            body: JSON.stringify({
+                login,
+                password
+            }),
+            headers: {
+                'Content-type': 'application/json;'
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(data);
+        }).catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+    }
 
     return (
-        <UserContext.Provider value={{user, setUser, handleSignUp,handleLogin}}>
+        <UserContext.Provider value={{user, setUser, handleSignUp,handleLogin,userCreation, setUserCreation}}>
             {children}
         </UserContext.Provider>
     )
--- config.js
+++ config.js
@@ -2,6 +2,13 @@ const settings = {
     backendURLLocal: "http://localhost:2023",
     backendURLRemote:"https://735b-105-178-112-193.ngrok.io"
 }
+const status = {
+    LOADING: 1,
+    DONE: 2,
+    ERROR: 3,
+    NOTHING: 4
+};
 export default {
-    backendURL:settings.backendURLRemote
+    backendURL:settings.backendURLLocal,
+    status
 }
